d6166006a4c430ad9ab1134ffeb15b4664040e80
Antipov Michael <antipov.me@yandex.ru>
2024-11-05 16:36:31 +0300

    add: Slider script

---
+++ portfolio_ame/assets/scripts/slider.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slider = document.querySelector(".slider");
+    const hiddenSliderItem = document.querySelector(".hidden");
+    const timerSlider = 10;
+
+})
